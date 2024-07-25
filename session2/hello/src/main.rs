95686dd242dc8066e1d95e5da8f7c215833c7c97
Lucas Santos <lucas.santoss1701@gmail.com>
2024-07-25 19:51:12 -0300

    feat: spawning threads

---
+++ session2/hello/src/main.rs
@@ -0,0 +1,17 @@
+
+fn hello_thread(n: u32){
+    println!("Hello from thread {n}");
+}
+
+fn main() {
+    println!("Hello with from the main thread");
+
+    let mut thread_handles = Vec::new();
+
+    for i in 0..5{
+        let thread_handle = std::thread::spawn(move || hello_thread(i));
+        thread_handles.push(thread_handle)
+    }
+
+    thread_handles.into_iter().for_each(|h| h.join().unwrap());
+}
