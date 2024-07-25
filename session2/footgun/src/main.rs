aad82d9226fa8747fd9b09a142dd550ba4743dbe
Lucas Santos <lucas.santoss1701@gmail.com>
2024-07-25 20:56:36 -0300

    feat: atomic operations

--- session2/footgun/src/main.rs
+++ session2/footgun/src/main.rs
@@ -1,16 +1,15 @@
 use std::sync::atomic::AtomicI32;
+use std::sync::atomic::Ordering::Relaxed;
 
-static mut COUNTER: i32 = 0;
-// static COUNTER: AtomicI32 = AtomicI32::new(0);
+// static mut COUNTER: i32 = 0;
+static COUNTER: AtomicI32 = AtomicI32::new(0);
 
 fn main() {
     let mut handles = Vec::new();
     for _ in 0..1000{
         let handle = std::thread::spawn(||{
             for _ in 0..1_1000{
-                unsafe {
-                    COUNTER += 1;
-                }
+                COUNTER.fetch_add(1, Relaxed);
             }
         });
         handles.push(handle);
@@ -20,7 +19,5 @@ fn main() {
        h.join().unwrap()
     });
 
-    unsafe {
-        println!("{COUNTER}")
-    }
+    println!("{}", COUNTER.load(Relaxed))
 }
