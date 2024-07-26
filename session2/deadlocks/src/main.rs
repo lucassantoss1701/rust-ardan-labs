8106529f4502645883c70f85f7feeb72bbc48e1c
Lucas Santos <lucas.santoss1701@gmail.com>
2024-07-26 19:05:16 -0300

    feat: poisoner

--- session2/deadlocks/src/main.rs
+++ session2/deadlocks/src/main.rs
@@ -2,19 +2,24 @@ use std::sync::Mutex;
 
 static MY_SHARED: Mutex<u32> = Mutex::new(3);
 
-// fn poisoner() {
-//     let mut lock = MY_SHARED.lock().unwrap();
-//     *lock += 1;
-//     panic!("The poisoner strikes")
-// }
+fn poisoner() {
+    let mut lock = MY_SHARED.lock().unwrap();
+    *lock += 1;
+    panic!("The poisoner strikes");
+}
 
 fn main() {
+    let handle = std::thread::spawn(poisoner);
+    println!("Trying to return from the thread");
+    println!("{:?}", handle.join());
 
-    let lock = MY_SHARED.lock().unwrap();
-    std::mem::drop(lock);
-    if let Ok(_lock) = MY_SHARED.try_lock() {
-        println!("Got the lock")
-    } else {
-        println!("No lock")
-    }
+    let lock = MY_SHARED.lock();
+    println!("{lock:?}");
+
+    let recovered_data = lock.unwrap_or_else(|poisoned| {
+        println!("Mutex was poisoned, recovering data...");
+        poisoned.into_inner()
+    });
+
+    println!("{recovered_data:?}")
 }
