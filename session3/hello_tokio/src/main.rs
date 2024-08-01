9ac873fb35baad41d70cede30b7392118104babe
Lucas Santos <lucas.santoss1701@gmail.com>
2024-08-01 19:49:38 -0300

    feat: hello tokio

---
+++ session3/hello_tokio/src/main.rs
@@ -0,0 +1,23 @@
+use tokio::runtime;
+
+async fn hello(){
+    println!("Hello tokio");
+}
+
+
+
+#[tokio::main]
+async fn main() {
+    hello().await
+}
+
+
+// fn main() {
+//     let rt = runtime::Builder::new_multi_thread()
+//         .enable_all()
+//         .worker_threads(4)
+//         .build()
+//         .unwrap();
+// 
+//     rt.block_on(hello());
+// }
