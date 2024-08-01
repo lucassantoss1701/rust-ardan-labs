3cdc020e3808ef74403b924ba36c55535ebe0ec3
Lucas Santos <lucas.santoss1701@gmail.com>
2024-08-01 20:07:55 -0300

    feat: yield and macros

--- session3/hello_tokio/src/main.rs
+++ session3/hello_tokio/src/main.rs
@@ -1,14 +1,44 @@
-use tokio::runtime;
+use tokio::{join, runtime, task};
 
-async fn hello(){
+async fn hello() -> u32{
     println!("Hello tokio");
+    3
 }
 
+async fn hello2() -> u32{
+    println!("Hello Tokio2");
+    4
+}
 
+async fn ticker(){
+    for i in 0..10{
+        println!(
+            "tick {i}"
+        );
+        task::yield_now().await;
+    }
+}
 
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() {
-    hello().await
+
+    // let result = join!(hello(), hello2());
+    // println!("{result:?}");
+    // let (one, two) = result;
+    // println!("{one}");
+    // println!("{two}");
+
+    // tokio::spawn(ticker());
+    // hello().await;
+
+    let _ = join!(
+        tokio::spawn(hello()),
+        tokio::spawn(ticker()),
+        tokio::spawn(ticker()),
+    );
+
+    println!("Finished");
+
 }
 
 
@@ -18,6 +48,6 @@ async fn main() {
 //         .worker_threads(4)
 //         .build()
 //         .unwrap();
-// 
+//
 //     rt.block_on(hello());
 // }
