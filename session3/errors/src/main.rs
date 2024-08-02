604710ce848642e43d55fb1bb81a866e946290b1
Lucas Santos <lucas.santoss1701@gmail.com>
2024-08-02 17:55:13 -0300

    feat: errors and generic errors

---
+++ session3/errors/src/main.rs
@@ -0,0 +1,44 @@
+use std::io::ErrorKind;
+use std::path::Path;
+use serde::Deserialize;
+
+fn maybe_read_a_file() -> Result<String, std::io::Error> {
+    let my_file = Path::new("myfile.txt");
+    std::fs::read_to_string(my_file)
+}
+
+fn file_to_uppercase() -> Result<String, std::io::Error> {
+    let contents =maybe_read_a_file()?;
+    Ok(contents.to_uppercase())
+}
+
+#[derive(Deserialize)]
+struct User{
+    user: String
+}
+
+type GenericResult<T> = Result<T, Box<dyn std::error::Error>>;
+
+fn load_users() -> GenericResult<Vec<User>>{
+    let my_path = Path::new("users.json");
+    let raw_text = std::fs::read_to_string(my_path)?;
+    let users: Vec<User> = serde_json::from_str(&raw_text)?;
+    Ok(users)
+}
+
+
+fn main() {
+    if let  Ok(content) = file_to_uppercase(){
+      println!("Contets: {content}")
+    };
+
+    let my_file = Path::new("myfile.txt");
+    let content = std::fs::read_to_string(my_file);
+    match content {
+        Ok(contents) => println!("{contents}"),
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => println!("File not found - myfile.txt"),
+            _ => println!("Error: {e:#?}")
+        },
+    };
+}
