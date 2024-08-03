ef08dc708fe562b139717a1cbe866c08cefd52a5
Lucas Santos <lucas.santoss1701@gmail.com>
2024-08-03 14:13:23 -0300

    feat: fileio async

--- session3/fileio/src/main.rs
+++ session3/fileio/src/main.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
+use tokio::io::AsyncBufReadExt;
+use tokio::time::Instant;
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
@@ -9,10 +11,10 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn main() {
+async fn line_count(filename: String) -> anyhow::Result<usize>{
     let now = std::time::Instant::now();
     let mut line_count = 0;
-    if let Ok(lines) =  read_lines("../warandpeace.txt"){
+    if let Ok(lines) =  read_lines(filename){
         lines.for_each(|line|{
             if let Ok(line) = line{
                 if !line.trim().is_empty(){
@@ -22,6 +24,47 @@ fn main() {
         });
     }
 
-    println!("Read {} lines in {:?} seconds", line_count, now.elapsed().as_secs_f32());
+    println!("Read {} lines in {:.3} seconds", line_count, now.elapsed().as_secs_f32());
+    Ok(line_count)
 }
 
+async fn async_line_count(filename: String) -> anyhow::Result<usize>{
+    use tokio::io::AsyncBufReadExt;
+    use tokio::io::BufReader;
+    use tokio::fs::File;
+
+    println!("Reading {filename}...");
+    let now = Instant::now();
+    let mut line_count = 0;
+
+    let file = File::open(filename).await?;
+    let reader = BufReader::new(file);
+    let mut lines = reader.lines();
+
+    while let Some (line) = lines.next_line().await?{
+        if !line.trim().is_empty(){
+            line_count += 1;
+        }
+    }
+
+    println!("Read {} lines in {:.3} seconds", line_count, now.elapsed().as_secs_f32());
+    Ok(line_count)
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()>{
+    println!("Reading warandpeace...");
+    let now = std::time::Instant::now();
+    let (c1,c2, ..) = tokio::join!(
+        async_line_count("../warandpeace.txt".to_string()),
+        async_line_count("../warandpeace.txt".to_string()),
+        async_line_count("../warandpeace.txt".to_string()),
+        async_line_count("../warandpeace.txt".to_string()),
+        async_line_count("../warandpeace.txt".to_string()),
+
+    );
+
+    println!("Total lines {}", c1? + c2?);
+    println!("In {:.3} seconds", now.elapsed().as_secs_f32());
+    Ok(())
+}
