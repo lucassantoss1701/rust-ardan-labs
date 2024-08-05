843a6cb8170392e1745916f2b42886fda7d6db32
Lucas Santos <lucas.santoss1701@gmail.com>
2024-08-05 20:29:33 -0300

    feat: tcp server hello world

--- session3/tcpserver/src/main.rs
+++ session3/tcpserver/src/main.rs
@@ -1,11 +1,31 @@
+use std::time::Duration;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpListener,
     spawn,
 };
+use tokio::net::TcpStream;
+
+async fn tcp_client() -> anyhow::Result<()>{
+    let mut stream = TcpStream::connect("127.0.0.1:8123").await?;
+    println!("Connected to the server");
+    stream.write_all(b"Hello World").await?;
+    let mut buf = vec![0u8;1024];
+    let bytes_read = stream.read(&mut buf).await?;
+    println!("Response: {}", String::from_utf8_lossy(&buf[..bytes_read]));
+    Ok(())
+}
+
+async fn client_runner(){
+    loop {
+        tokio::time::sleep(Duration::from_nanos(1)).await;
+        let _ = tcp_client().await;
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    spawn(client_runner());
     let listener = TcpListener::bind("127.0.0.1:8123").await?;
 
     loop{
