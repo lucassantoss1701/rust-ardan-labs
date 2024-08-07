f4f32572df60c75d5883f273af39ba79c020b4d5
Lucas Santos <lucas.santoss1701@gmail.com>
2024-08-07 20:51:36 -0300

    feat: pinning memory

--- session3/recursion/src/main.rs
+++ session3/recursion/src/main.rs
@@ -1,3 +1,5 @@
+use std::future::Future;
+use std::pin::Pin;
 use async_recursion::*;
 
 #[async_recursion]
@@ -9,7 +11,41 @@ async fn fibonacci(n: u32) -> u32{
     }
 }
 
+async fn one() {
+    println!("one");
+}
+
+async fn two() {
+    println!("two");
+}
+
+async fn call_one_of_them(n: u32) -> Pin<Box<dyn Future<Output = ()>+ Send>>{
+  match n {
+      1 => Box::pin(one()),
+      2 => Box::pin(two()),
+      _ => panic!("Invalid choice")
+  }
+}
+
 #[tokio::main]
 async fn main() {
     println!("fibonacci(10) = {}", fibonacci(10).await);
+
+    let  mut future = async {
+        println!("Hello World");
+    };
+
+    tokio::pin!(future);
+    (&mut future).await;
+
+
+
+    let spawned = tokio::spawn( async {
+        for n in 1..=3{
+            let pin_call = call_one_of_them(n).await;
+            tokio::spawn(pin_call);
+        }
+    });
+
+    spawned.await.unwrap();
 }
