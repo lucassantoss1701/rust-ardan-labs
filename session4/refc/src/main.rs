c55ac866fd411514fe9967fed61d316ce6d1f5cc
Lucas Santos <lucas.santoss1701@gmail.com>
2024-08-14 18:21:13 -0300

    feat: refc with internal mutext

--- session4/refc/src/main.rs
+++ session4/refc/src/main.rs
@@ -1,20 +1,30 @@
 use std::sync::{Arc, Mutex};
 
-struct SharedData(String);
+struct SharedData{
+    data: Mutex<String>,
+}
+
+impl SharedData {
+    fn new(s: &str) -> Self {
+        Self {
+            data: Mutex::new(s.to_string()),
+        }
+    }
+}
 
 fn main() {
-    let my_shared = Arc::new(Mutex::new(SharedData("Hello".to_string())));
+    let my_shared = Arc::new(SharedData::new("Hello"));
     let mut threads = Vec::new();
     for i in 0..10 {
         let my_shared = my_shared.clone();
         threads.push(std::thread::spawn(move || {
-            let mut data = my_shared.lock().unwrap();
-            data.0.push_str(&format!(" {i}"));
+            let mut data = my_shared.data.lock().unwrap();
+            data.push_str(&format!(" {i}"));
         }));
     }
     for t in threads {
         t.join().unwrap();
     }
-    let data = my_shared.lock().unwrap();
-    println!("{}", data.0);
+    let data = my_shared.data.lock().unwrap();
+    println!("{data}");
 }
